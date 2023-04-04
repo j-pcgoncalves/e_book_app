c5e7f8d7eafa9dc431075c544421d00c490b8604
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-04-04 19:31:16 +0100

    style ReadingListCard

--- lib/widgets/reading_list_card.dart
+++ lib/widgets/reading_list_card.dart
@@ -1,3 +1,6 @@
+import 'package:e_book_app/constants.dart';
+import 'package:e_book_app/widgets/book_rating.dart';
+import 'package:e_book_app/widgets/two_side_rounded_button.dart';
 import 'package:flutter/material.dart';
 
 class ReadingListCard extends StatelessWidget {
@@ -20,6 +23,107 @@ class ReadingListCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      margin: const EdgeInsets.only(left: 24, bottom: 40),
+      height: 245,
+      width: 202,
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 221,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(29),
+                boxShadow: [
+                  BoxShadow(
+                    offset: const Offset(0, 10),
+                    blurRadius: 33,
+                    color: kShadowColor,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Image.asset(
+            image,
+            width: 150,
+          ),
+          Positioned(
+            top: 35,
+            right: 10,
+            child: Column(
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(
+                    Icons.favorite_border,
+                  ),
+                  onPressed: () {},
+                ),
+                BookRating(score: rating),
+              ],
+            ),
+          ),
+          Positioned(
+            top: 160,
+            child: Container(
+              height: 85,
+              width: 202,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(left: 24),
+                    child: RichText(
+                      maxLines: 2,
+                      text: TextSpan(
+                        style: const TextStyle(color: kBlackColor),
+                        children: [
+                          TextSpan(
+                            text: '$title\n',
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          TextSpan(
+                            text: auth,
+                            style: const TextStyle(
+                              color: kLightBlackColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const Spacer(),
+                  Row(
+                    children: <Widget>[
+                      GestureDetector(
+                        onTap: pressDetails,
+                        child: Container(
+                          width: 101,
+                          padding: const EdgeInsets.symmetric(vertical: 10),
+                          alignment: Alignment.center,
+                          child: const Text('Details'),
+                        ),
+                      ),
+                      Expanded(
+                        child: TwoSideRoundedButton(
+                          text: 'Read',
+                          press: pressRead,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
