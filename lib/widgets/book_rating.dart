b4ff497ebcd0cd1a34b1c87110b4f51b61e153bb
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-04-04 19:40:06 +0100

    style BookRating

--- lib/widgets/book_rating.dart
+++ lib/widgets/book_rating.dart
@@ -1,3 +1,4 @@
+import 'package:e_book_app/constants.dart';
 import 'package:flutter/material.dart';
 
 class BookRating extends StatelessWidget {
@@ -15,6 +16,30 @@ class BookRating extends StatelessWidget {
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            offset: const Offset(3, 7),
+            blurRadius: 20,
+            color: const Color(0xFD3D3D3D).withOpacity(.5),
+          ),
+        ],
+      ),
+      child: Column(
+        children: <Widget>[
+          const Icon(
+            Icons.star,
+            color: kIconColor,
+            size: 15,
+          ),
+          const SizedBox(height: 5),
+          Text(
+            '$score',
+            style: const TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
       ),
     );
   }
